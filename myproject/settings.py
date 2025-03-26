67097de38c4969b953ab3ec44a5c5933c5a003eb
bekzodbek01 <bekzodbek5201@gmail.com>
2025-03-26 16:07:41 +0500

    1

--- myproject/settings.py
+++ myproject/settings.py
@@ -98,8 +98,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
